c1c0d285977539e86d31d1d425616797a16a4c91
samiul islam <78860039+samiulabir201@users.noreply.github.com>
2024-05-08 16:48:00 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p , .about-content', { origin: 'right' });
 
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Data Engineer', 'Machine Learning Enthusiast', 'Competitive Programmer', 'Software developer'],
+    strings: ['BI Analyst', 'Data Engineer', 'Machine Learning Enthusiast'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
